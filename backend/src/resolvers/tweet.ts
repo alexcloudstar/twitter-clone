2b28e5ca91020439c02985bc9bc96131834b4a5f
alexcloudstar <alexcloudstar@gmail.com>
2021-09-21 15:15:41 +0300

    Added get tweet by id & getTweets queries

--- backend/src/resolvers/tweet.ts
+++ backend/src/resolvers/tweet.ts
@@ -1,4 +1,12 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+} from 'type-graphql';
 import { Tweet } from '../entities/Tweet';
 import { User } from '../entities/User';
 import { MyContext } from '../types/MyContext';
@@ -11,17 +19,36 @@ export class TweetFields {
 
 @Resolver(Tweet)
 export class TweetResolver {
+  @Query(() => [Tweet])
+  async getTweets(): Promise<Tweet[]> {
+    const allTweets = await Tweet.find();
+    console.log(allTweets);
+
+    return allTweets;
+  }
+  @Query(() => Tweet)
+  async getTweet(@Arg('tweetId') tweetId: number): Promise<Tweet | undefined> {
+    let selectedTweet;
+
+    try {
+      selectedTweet = await Tweet.findOne(tweetId);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+
+    return selectedTweet;
+  }
   @Mutation(() => Tweet)
   async createTweet(
     @Arg('options') options: TweetFields,
     @Ctx() { req }: MyContext
-  ): Promise<Tweet> {
+  ) {
     const creator = await User.findOne(req.session.userId);
 
     return Tweet.create({
       ...options,
       creatorId: req.session.uerId,
       creator: { ...creator },
-    });
+    }).save();
   }
 }
