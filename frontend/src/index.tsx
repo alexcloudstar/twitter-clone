db661aa5ebfc6f5b3642318dbd3da6d5f4031c4f
alexcloudstar <alexcloudstar@gmail.com>
2021-09-29 20:59:23 +0300

    Added credentials: include to activate cookies

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,8 @@ import GlobalStyles from './Theme/globalStyles';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4000/graphql',
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
+	credentials: 'include'
 });
 
 ReactDOM.render(
