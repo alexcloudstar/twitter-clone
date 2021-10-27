762b38ab07872c5bc8368364725eeb41320cedeb
alexcloudstar <alexcloudstar@gmail.com>
2021-10-27 09:53:09 +0300

    Added Barebones of Profile Container

---
+++ frontend/src/containers/Profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import { ProfileWrapper } from './style';
+import { ProfileProps } from './types';
+
+const Profile: FC<ProfileProps> = () => {
+	return <ProfileWrapper>Profile page</ProfileWrapper>;
+};
+
+export default Profile;
